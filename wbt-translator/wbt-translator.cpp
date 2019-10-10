7e61b2760aa6fcacb475766c9283d36ca64630a9
Simon Virenfeldt <simwir@hotmail.com>
2019-10-10 08:32:12 +0200

    Removed using namespace std from wbt-translator.cpp

--- wbt-translator/wbt-translator.cpp
+++ wbt-translator/wbt-translator.cpp
@@ -6,12 +6,9 @@
 #include "webots_parser.hpp"
 #include "distance_matrix.hpp"
 
-using namespace std;
-
-string inputFile;
-
 int main(int argc, char **argv)
 {
+    std::string inputFile;
     if (argc == 1 || argc > 2) {
         std::cout << "Usage: " << argv[0] << " INPUT_FILE\n";
         exit(0);
@@ -19,12 +16,12 @@ int main(int argc, char **argv)
 
     inputFile = argv[1];
 
-    ifstream infile{inputFile};
+    std::ifstream infile{inputFile};
     if (!infile.is_open()) {
-        cout << "The file " << inputFile << " could not be opened.\n";
+        std::cout << "The file " << inputFile << " could not be opened.\n";
         exit(1);
     }
     AST ast = Parser{infile}.parse_stream();
     distance_matrix dist{ast};
-    cout << dist.to_uppaal_declaration();
+    std::cout << dist.to_uppaal_declaration();
 }
