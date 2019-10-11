6ad537e28f2e34860d948e38f22c91577ffe6bce
Nikolaj Jensen Ulrik <nulrik16@student.aau.dk>
2019-10-11 12:34:43 +0200

    More concise loops

--- wbt-translator/apsp.cpp
+++ wbt-translator/apsp.cpp
@@ -10,26 +10,26 @@ std::map<int, std::map<int, double>> all_pairs_shortest_path(const AST& ast){
     std::map<int, std::map<int, double>> dist;
 
     // Initialise dist to DBL_MAX
-    for(auto rowIt = ast.nodes.begin(); rowIt != ast.nodes.end(); rowIt++){
-        std::map<int, double> col{};
-        for(auto colIt = ast.nodes.begin(); colIt != ast.nodes.end(); colIt++){
-            col[colIt->first] = DBL_MAX;
+
+    for (auto &[rowId, _] : ast.nodes) {
+        std::map<int, double> column;
+        for (auto &[colId, _] : ast.nodes) {
+            column[colId] = DBL_MAX;
         }
-        dist[rowIt->first] = col;
+        dist[rowId] = std::move(column);
     }
 
     // For each edge set the distance between the verts to the edge length.
-    for(auto nodeIt = ast.nodes.begin(); nodeIt != ast.nodes.end(); nodeIt++){
-        Waypoint from = nodeIt->second;
-        for(auto adjIt = from.adjList.begin(); adjIt != from.adjList.end(); adjIt++){
-            Waypoint to = ast.nodes.at(*adjIt);
-            dist[from.id][to.id] = euclidean_distance(from, to);
+    for (auto &[_, from] : ast.nodes) {
+        for (auto adjId : from.adjList) {
+            Waypoint to = ast.nodes.at(adjId);
+            dist.at(from.id).at(adjId) = euclidean_distance(from, to);
         }
     }
 
     // For each vertex set the distance to yourself to 0
-    for(auto nodeIt = ast.nodes.begin(); nodeIt != ast.nodes.end(); nodeIt++){
-        dist[nodeIt->first][nodeIt->first] = 0.0;
+    for (auto &[nodeId, _] : ast.nodes) {
+        dist.at(nodeId).at(nodeId) = 0.0;
     }
 
     // Calculate all pairs shortest path
