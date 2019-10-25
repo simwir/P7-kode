c1deae7dd76ecb0644961758f109abbf64f05fd8
Emil Bækdahl <emil@progras.dk>
2019-10-25 13:32:44 +0200

    Add function macro for defining simple config getter functions in C wrapper

--- robot/config_wrapper.cpp
+++ robot/config_wrapper.cpp
@@ -8,23 +8,12 @@ void set_config_path(char *file_path)
 {
     config.load_from_file(file_path);
 }
-int number_of_stations()
-{
-    return config.get<int>("number_of_stations");
-}
-int number_of_end_stations()
-{
-    return config.get<int>("number_of_end_stations");
-}
-int number_of_robots()
-{
-    return config.get<int>("number_of_robots");
-}
-int number_of_waypoints()
-{
-    return config.get<int>("number_of_waypoints");
-}
-double uncertainty()
-{
-    return config.get<double>("uncertainty");
-}
+
+CONFIG_GETTER(int, number_of_stations);
+CONFIG_GETTER(int, number_of_end_stations);
+CONFIG_GETTER(int, number_of_robots);
+CONFIG_GETTER(int, number_of_waypoints);
+CONFIG_GETTER(int, waypoint_passing_time);
+CONFIG_GETTER(int, station_passing_time);
+CONFIG_GETTER(int, current_station);
+CONFIG_GETTER(double, uncertainty);
--- robot/config_wrapper.h
+++ robot/config_wrapper.h
@@ -1,6 +1,9 @@
 #ifndef CONFIG_WRAPPER_HPP
 #define CONFIG_WRAPPER_HPP
 
+#define CONFIG_GETTER(type, key)                                                                   \
+    type key() { return config.get<type>(#key); }
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -10,7 +13,15 @@ int number_of_stations();
 int number_of_end_stations();
 int number_of_robots();
 int number_of_waypoints();
+int waypoint_passing_time();
+int station_passing_time();
+int current_station();
 double uncertainty();
+void station_distance_matrix(int number_of_stations,
+                             int matrix[number_of_stations][number_of_stations]);
+void way_point_distance_matrix(int number_of_waypoints,
+                               int matrix[number_of_waypoints][number_of_waypoints]);
+void other_robot_schedules();
 
 #ifdef __cplusplus
 }
