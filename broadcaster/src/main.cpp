821223c63a405327679af768e4f8799d602298d3
Anders <amads15@student.aau.dk>
2019-11-05 10:36:11 +0100

    removed debugging code

--- broadcaster/src/main.cpp
+++ broadcaster/src/main.cpp
@@ -1,11 +1,5 @@
 #include <broadcaster/broadcaster.hpp>
 
-std::vector<std::string> split_message(const std::string message){
-    std::string start = message.substr(0, message.find(','));
-    std::string end = message.substr(message.find(',') + 1);
-    return std::vector<std::string> {start, end};
-}
-
 int main(int argc, char **argv)
 {
     broadcaster::Broadcaster broadcaster(0);
