389260d8d4e7bace4016c30dc9697974048ccb6f
Anders <amads15@student.aau.dk>
2019-11-05 10:35:18 +0100

    removed debugging line

--- broadcaster/src/main.cpp
+++ broadcaster/src/main.cpp
@@ -8,9 +8,6 @@ std::vector<std::string> split_message(const std::string message){
 
 int main(int argc, char **argv)
 {
-    /*std::string teststring = "#|post_robot_location,{\"id\":1,\"location\":{\"x\":1.1, \"y\":2.2},\"station_plan\":[1,2,3,4,5],\"waypoint_plan\":[3,1,2,3,5,6],\"eta\":2.42}|#";
-    std::vector<std::string> result = split_message(teststring);
-    robot::Info info = robot::Info::from_json(result[0]);*/
     broadcaster::Broadcaster broadcaster(0);
     broadcaster.start_broadcasting();
 
