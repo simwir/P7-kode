52f183fe556c5ce83523c8fd2dd84f12c727fdb5
Nikolaj Jensen Ulrik <nulrik16@student.aau.dk>
2019-11-08 09:29:53 +0100

    Make atomic version of pollable

--- util/include/util/pollable.hpp
+++ util/include/util/pollable.hpp
@@ -1,45 +1,53 @@
 #ifndef UTIL_POLLABLE_HPP
 #define UTIL_POLLABLE_HPP
 
+#include <mutex>
+#include <type_traits>
+
 /**
  * Wrapper class around a value that can be updated from elsewhere and be polled for
  * whether such updates have happened.
  */
-template <typename T>
-class Pollable {
+template <typename T, bool atomic = false>
+class _Pollable {
     T value;
     bool dirty = false;
+    std::enable_if<atomic, std::mutex> mutex;
 
-public:
-    Pollable() = default;
-    Pollable(T &&value) : value(value), dirty(false) {}
-
-    Pollable(const Pollable<T> &) = default;
-    Pollable(Pollable<T> &&) = default;
-    Pollable<T> &operator=(Pollable<T> &&) = default;
-    Pollable<T> &operator=(const Pollable<T> &) = default;
+  public:
+    _Pollable() = default;
+    _Pollable(T &&value) : value(value), dirty(false) {}
 
     bool is_dirty() const { return dirty; }
 
     void reset(T &&value)
     {
+        if constexpr (atomic) {
+            std::scoped_lock lock{mutex};
+        }
         dirty = true;
         this->value = value;
     }
 
     void reset(const T &value)
     {
+        if constexpr (atomic) {
+            std::scoped_lock lock{mutex};
+        }
         dirty = true;
         this->value = value;
     }
 
     T get()
     {
+        if constexpr (atomic) {
+            std::scoped_lock lock{mutex};
+        }
         dirty = false;
         return value;
     }
 
-    Pollable<T> &operator=(const T &t)
+    _Pollable<T> &operator=(const T &t)
     {
         reset(t);
         return *this;
@@ -51,6 +59,12 @@ public:
 
 // Deduction guide
 template <typename T>
-Pollable(T) -> Pollable<T>;
+_Pollable(T)->_Pollable<T>;
+
+template <typename T>
+using AtomicPollable = _Pollable<T, true>;
+template <typename T>
+using Pollable = _Pollable<T, false>;
+
 
 #endif
