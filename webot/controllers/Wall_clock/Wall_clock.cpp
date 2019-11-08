27a87eb013abe4b26fe8016cccc018275dd4f48c
Simon Virenfeldt <simwir@hotmail.com>
2019-11-08 11:11:46 +0100

    Format Wall_clock.cpp

--- webot/controllers/Wall_clock/Wall_clock.cpp
+++ webot/controllers/Wall_clock/Wall_clock.cpp
@@ -1,26 +1,28 @@
-#include <webots/Robot.hpp>
 #include <tcp/server.hpp>
 #include <thread>
+#include <webots/Robot.hpp>
 
 #include <iostream>
 
 using namespace webots;
 
 void connection(std::shared_ptr<tcp::Connection> connection, Robot *robot);
-void accepter(Robot* robot);
+void accepter(Robot *robot);
 
-int main(int argc, char **argv) {
-  Robot *robot = new Robot();
-  int time_step = (int)robot->getBasicTimeStep();
-  std::thread accept_thread{accepter, robot};
-  while (robot->step(time_step) != -1) {
-  };
+int main(int argc, char **argv)
+{
+    Robot *robot = new Robot();
+    int time_step = (int)robot->getBasicTimeStep();
+    std::thread accept_thread{accepter, robot};
+    while (robot->step(time_step) != -1) {
+    };
 
-  delete robot;
-  return 0;
+    delete robot;
+    return 0;
 }
 
-void accepter(Robot* robot){
+void accepter(Robot *robot)
+{
     tcp::Server server{5555};
     while (true) {
         auto conn = server.accept();
