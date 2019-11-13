dc940cf74201a0b6757085dadbf386d12da3b6ba
Anders <amads15@student.aau.dk>
2019-11-13 15:31:55 +0100

    clang format

--- communication/src/main.cpp
+++ communication/src/main.cpp
@@ -4,8 +4,8 @@
 int main(int argc, char **argv)
 {
     if (argc != 2) {
-        std::cout << "Please specify what port the communication module  is run on. Use 0 for OS to assign "
-                     "any available port."
+        std::cout << "Please specify what port the communication module  is run on. Use 0 for OS "
+                     "to assign any available port."
                   << std::endl;
         exit(0);
     }
