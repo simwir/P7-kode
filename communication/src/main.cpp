e79b725f38f021ee80ecebcc4f7a73724d0b2e05
Anders <amads15@student.aau.dk>
2019-11-13 12:56:38 +0100

    renamed broadcaster to com-module

--- communication/src/main.cpp
+++ communication/src/main.cpp
@@ -4,12 +4,12 @@
 int main(int argc, char **argv)
 {
     if (argc != 2) {
-        std::cout << "Please specify what port the broadcaster is run on. Use 0 for OS to assign "
+        std::cout << "Please specify what port the communication module  is run on. Use 0 for OS to assign "
                      "any available port."
                   << std::endl;
         exit(0);
     }
-    communication::ComModule broadcaster(std::stoi(argv[1]));
-    broadcaster.start_broadcasting();
+    communication::ComModule com_module(std::stoi(argv[1]));
+    com_module.start_broadcasting();
     return 0;
 }
