ffca1a7e2080a68234da9effae628dc3d3fb2f3e
Frederik Mathiesen <frederik_mathies@hotmail.com>
2019-11-15 11:53:05 +0100

    Include jsoncpp

--- util/include/util/json.hpp
+++ util/include/util/json.hpp
@@ -19,11 +19,13 @@
 #ifndef JSON_HEADER_WRAPPER_HPP
 #define JSON_HEADER_WRAPPER_HPP
 
-#if __has_include("json/json.h")
+/*#if __has_include("json/json.h")
 #include <json/json.h>
 #else
 #include <jsoncpp/json/json.h>
-#endif
+#endif*/
+
+#include "json/json.h"
 
 struct JsonConversionException : public std::exception {
     const char *what() const noexcept override { return "Cannot convert json to object"; }
