62d5e6cc03cc793259873b60e0e1c721d22ff6a5
Frederik Mathiesen <frederik_mathies@hotmail.com>
2019-11-15 11:56:57 +0100

    Remove unneeded include

--- util/include/util/json.hpp
+++ util/include/util/json.hpp
@@ -19,12 +19,6 @@
 #ifndef JSON_HEADER_WRAPPER_HPP
 #define JSON_HEADER_WRAPPER_HPP
 
-/*#if __has_include("json/json.h")
-#include <json/json.h>
-#else
-#include <jsoncpp/json/json.h>
-#endif*/
-
 #include "json/json.h"
 
 struct JsonConversionException : public std::exception {
