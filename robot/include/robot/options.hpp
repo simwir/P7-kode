47e3290136450dfdfd0773ab1071cb5d8d14f7c2
Simon Virenfeldt <simwir@hotmail.com>
2019-12-06 09:38:58 +0100

    Added -a to orchestrator options

--- robot/include/robot/options.hpp
+++ robot/include/robot/options.hpp
@@ -44,7 +44,7 @@ struct Options {
     std::filesystem::path eta_query_template = std::filesystem::path("eta_query.q.template");
 };
 
-const char *const shortOpts = "t:sc:p:o:hr:q";
+const char *const shortOpts = "t:sc:p:o:hr:qa:";
 const option longOpts[] = {{"time-service", required_argument, nullptr, 't'},
                            {"system-time", no_argument, nullptr, 's'},
                            {"com-module", required_argument, nullptr, 'c'},
@@ -53,6 +53,7 @@ const option longOpts[] = {{"time-service", required_argument, nullptr, 't'},
                            {"help", no_argument, nullptr, 'h'},
                            {"robot", required_argument, nullptr, 'r'},
                            {"station-query-file", required_argument, nullptr, 'q'},
-                           {"waypoint-query-file", required_argument, nullptr, 'w'}};
+                           {"waypoint-query-file", required_argument, nullptr, 'w'},
+                           {"single-ip", required_argument, nullptr, 'a'}};
 } // namespace robot
 #endif
